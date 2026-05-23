abab573ff680196e181437edfa85188805bc23c7
wilburhsieh-stu <lingyue9688@gmail.com>
2026-05-23 19:41:40 +0800

    Add filtering for adult users in 07.js

---
+++ homework/06/07.js
@@ -0,0 +1,8 @@
+const users = [
+  {name: "Alice", age: 25}, 
+  {name: "Bob", age: 17}
+];
+
+const adults = users.filter(user => user.age >= 18);
+
+console.log(adults); // [{name: "Alice", age: 25}]
