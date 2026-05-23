41f195327f2046b01e213cd31d6f40039efb9577
wilburhsieh-stu <lingyue9688@gmail.com>
2026-05-23 19:41:12 +0800

    Create 05.js

---
+++ homework/06/05.js
@@ -0,0 +1,9 @@
+function multiplier(factor) {
+  return (n) => n * factor;
+}
+
+const double = multiplier(2);
+console.log(double(10)); // 20
+
+const triple = multiplier(3);
+console.log(triple(10)); // 30
